0d991888895d92c44b08db45c95eae6e7b3a946a
Ameya Shrivastava <ameyshri051@gmail.com>
2023-03-12 23:41:11 +0530

    sidebar

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "../screens/auth/AdminLogin";
+import Products from "../screens/dashboard/Products";
+import Private from "./Private";
 import Public from "./Public";
 
 const Router = () => {
@@ -13,6 +15,16 @@ const Router = () => {
               <Public>
                 <AdminLogin />
               </Public>
+            }
+          />
+        </Route>
+        <Route path="dashboard">
+          <Route
+            path="products"
+            element={
+              <Private>
+                <Products />
+              </Private>
             }
           />
         </Route>
